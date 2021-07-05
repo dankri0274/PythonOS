5d09f593106b5aa0ff2c474a9a8db8b5a91b05a3
dannyboy9934 <50834782+dannyboy9934@users.noreply.github.com>
2021-07-05 15:23:10 +0200

    massive changes

--- terminal.py
+++ terminal.py
@@ -1,29 +1,27 @@
+import os
 import sys
 import time
-import socket 
-import stdiomask
+import socket
 from os import name, system
+
+try:
+	import stdiomask
+except ImportError:
+	if name == "nt":
+		os.system("pip install stdiomask")
+	else:
+		os.system("pip3 install stdiomask")
 
 #*___________________SETUP___________________
 
 cmd = ""
+rootpassword = "admin"
 
 root = False
-host = socket.gethostname()
-ip = socket.gethostbyname(host)
 running = True
 
-def booting():
-	for _ in range(3):
-		sys.stdout.write("\rStarting up   ")
-		time.sleep(1)
-		sys.stdout.write("\rStarting up.  ")
-		time.sleep(1)
-		sys.stdout.write("\rStarting up.. ")
-		time.sleep(1)
-		sys.stdout.write("\rStarting up...")
-		time.sleep(1)
-
+host = socket.gethostname()
+ip = socket.gethostbyname(host)
 
 def symbol():
 	if root:
@@ -31,6 +29,7 @@ def symbol():
 	else:
 		return "$"
 
+#*Style and color for output text
 class style():
 	underline = '\033[4m' #!Styling
 	
@@ -45,6 +44,7 @@ class style():
 	red = '\033[31m'
 	reset = '\033[0m'
 
+#Checks if code is running on a Windows or a Linux OS
 def cls():
 	if name == "nt":
 		_ = system("cls")
@@ -55,8 +55,6 @@ cls()
 
 #*_________________________The program itself_________________________
 
-booting()
-
 cls()
 
 user = input("Enter name: ")
@@ -66,7 +64,7 @@ cls()
 
 username = input("Enter username: ")
 username = username.lower()
-"""
+
 cls()
 
 password = stdiomask.getpass(prompt = "Enter a password: ")
@@ -81,43 +79,101 @@ elif len(password) < 8 or len(passConf) < 8:
 	print(style.red + "Password must contain at least 8 characters" + style.reset)
 else:
 	print(style.red + "Passwords don't match" + style.reset)
-"""
+
 cls()
 
 while running:
-	cmd = input(style.green + f"{username}@{host}" + style.reset + f":~{symbol()} ")
+	cmd = input(f"{style.green + username}@{host + style.reset}:{style.blue}~{style.reset}{symbol()} ")
 	cmd = cmd.lower()
 
-	if cmd == "ip":
-		print(ip)
+	#*COMMANDS
+
+	#*Practical commands
+	if cmd == "-help":
+		print(
+			style.cyan +
+			"Commands:\n"
+			"\t1. su # = switch user to root user\n"
+			"\t2. ip = Get the local IPv4 address\n"
+			"\t3. clear / cls = clear screen\n"
+			"\t4. pcname = shows the name of the PC\n"
+			"\t5. whoami = shows if you are root user or local user\n"
+			"\t6. ping = enter ping, and a second line will appear where\n\t-you can enter a website or local address to ping\n"
+			"\t7. shutdown -now = quits the program"
+			+ style.reset
+		)
+	elif cmd == "ip":
+		print(style.blue + ip + style.reset)
 	elif cmd == "pcname":
-		print(f"This PC has the name: {host}")
-	elif cmd == "clear":
+		print(style.green + host + style.reset)
+	elif cmd == "clear" or cmd == "cls":
 		cls()
 	elif cmd == "su #":
 		root = True
 		cls()
-	elif cmd ==  "su":
+	elif cmd == "su":
 		root = False
-		cls()
-	elif cmd == "chg name" and not root:
-		print(style.red + "You must be root user to change owner name!" + style.reset)
-	elif cmd == "chg name" and root:
-		user = input("Enter new name: ")
-
-	elif cmd == "chg username" and not root:
-		print(style.red + "You must be root user to change username!" + style.reset)
-	elif cmd == "chg username" and root:
-		username = input("Enter new username: ")
-
 	elif cmd == "whoami":
 		if root:
 			print(f"{username} as root")
 		else:
-			print(f"{user} as user {username}")
+			print(f"{style.red + user + style.reset} as user {style.blue + username + style.reset}")
+	elif cmd == "ping":
+		pingaddress = input("Enter address to ping: ")
+		pingcmd = f"ping {pingaddress}"
+		os.system(pingcmd)
 	elif cmd == "shutdown -now":
-		print(style.yellow + "Shutting down..." + style.reset)
+		cls
+		print(style.cyan + "Made by Daniel Kristensen\nWritten in Python" + style.reset)
 		time.sleep(2)
 		exit()
+	
+
+	#*Commands that change system settings
+	
+	elif cmd == "chg name":
+		if root:
+			user = input("Enter new name: ")
+			user = user.title()
+			print(f"Name successfully changed to: {style.blue + user + style.reset}")
+			time.sleep(2)
+			cls()
+		else:
+			print(style.red + "You must be root user to change owner name!" + style.reset)
+
+	elif cmd == "chg username":
+		if root:
+			username = input("Enter new username: ")
+			print(f"Username successfully changed to: {style.blue + username + style.reset}")
+			time.sleep(2)
+			cls()
+		else:
+			print(style.red + "You must be root user to change username!" + style.reset)
+	
+
+	elif cmd == "list su cmd" and root:
+		print(
+			"Available commands:\n"
+			"\t1. chg username = change username of user\n"
+			"\t2. chg name = change name of user\n"
+			"\t3. chg password\n"
+		)
+
+	#*Root settings
+	elif cmd == "chg password":
+		if root:
+			password = stdiomask.getpass(prompt = "Enter new password: ", mask = "*")
+			passConf = stdiomask.getpass(prompt = "confirm new password: ", mask = "*")
+			if password == passConf:
+				print(style.green + f"Password for {username} successfully changed" + style.reset)
+				time.sleep(2)
+				cls()
+			else:
+				print(style.red + "Passwords don't match" + style.reset)
+		else:
+			print(style.red + "You must be root user to change password!")
+
+	#EOS
 	else:
 		print(style.red + "Unknown command!" + style.reset)
+		
